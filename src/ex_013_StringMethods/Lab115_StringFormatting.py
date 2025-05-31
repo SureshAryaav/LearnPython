aab4c90e03c64c54248c694833a961a5cb04a240
SureshAryaav <Suresharyaav@gmail.com>
2025-05-31 09:05:09 +0530

    Python - String Methods

---
+++ src/ex_013_StringMethods/Lab115_StringFormatting.py
@@ -0,0 +1,6 @@
+name = "Ravi"
+age = 25
+print(f"My name is {name} and I am {age} years old.")
+
+
+print("My name is {} and I am {} years old.".format(name, age))
---
+++ src/ex_013_StringMethods/Lab120_FindWordOccrance.py
@@ -0,0 +1,3 @@
+msg = "Python is fun. Python is easy."
+print("First 'Python' found at:", msg.find("Python"))
+print("Python count:", msg.count("Python"))
